c3a7bdb955b66e6d9aed26af8f2e95f1a110806c
Lucas Nunes <lnunessouza21@gmail.com>
2022-01-31 01:08:40 -0300

    implementando user e postagens edit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,9 @@ import { InicioComponent } from './inicio/inicio.component';
 import { TemaComponent } from './tema/tema.component';
 import { TemaEditComponent } from './edit/tema-edit/tema-edit.component';
 import { TemaDeleteComponent } from './delete/tema-delete/tema-delete.component';
+import { PostagemEditComponent } from './edit/postagem-edit/postagem-edit.component';
+import { PostagemDeleteComponent } from './delete/postagem-delete/postagem-delete.component';
+import { UserEditComponent } from './edit/user-edit/user-edit.component';
 
 @NgModule({
   declarations: [
@@ -25,6 +28,9 @@ import { TemaDeleteComponent } from './delete/tema-delete/tema-delete.component'
     TemaComponent,
     TemaEditComponent,
     TemaDeleteComponent,
+    PostagemEditComponent,
+    PostagemDeleteComponent,
+    UserEditComponent,
   ],
   imports: [
     BrowserModule,
